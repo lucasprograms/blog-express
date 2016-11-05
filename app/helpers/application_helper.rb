cf97eb152f9819bdad1f0f5ca2824d93eeaa214e
Lucas McCain <lucasprograms@gmail.com>
2016-11-04 19:40:46 -0500

    add full_title helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
+# helper for application.html (and others?)
 module ApplicationHelper
+  def full_title(page_title = '')
+    base_title = 'Blogex.press'
+    if page_title.empty?
+      base_title
+    else
+      "#{page_title} | #{base_title}"
+    end
+  end
 end
